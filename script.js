b6bce4d0176f3097051abfff4c6d7e9bab4525cf
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-31 17:01:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 //your JS code here. If required.
+const userNameinput=document.getElementById("username");
+const userPasswordinput=document.getElementById("password");
+const checkbox = document.getElementById("checkbox");
+const existingBtn = document.getElementById("existing");
+const form = document.getElementById("loginForm");
+
+
+const savedUsername=localStorage.getItem('username');
+const savedUserpassword=localStorage.getItem('password');
+
+if(savedUsername && savedUserpassword){
+	existingBtn.style.display="block";
+}
+
+form.addEventListener("submit",function (e) {
+	e.preventDefault();
+
+	const username=userNameinput.value;
+	const password=userPasswordinput.value;
+
+	alert("Logged in as " + username);
+
+	if(checkbox.checked){
+		 localStorage.setItem("username", username);
+	    localStorage.setItem("password", password);
+	}else{
+		 localStorage.removeItem("username");
+    localStorage.removeItem("password");
+	}
+
+
+	
+	 if (localStorage.getItem("username")) {
+    existingBtn.style.display = "block";
+		 
+  } else {
+    existingBtn.style.display = "none";
+  }
+});
+
+
+existingBtn.addEventListener("click",function () {
+	 const storedUser = localStorage.getItem("username");
+  if (storedUser) {
+    alert("Logged in as " + storedUser);
+  }
+})
